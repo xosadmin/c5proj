173db5c178498c192a5086a7b91071da2d01baaf
X-osadmin <9164356+xosadmin@users.noreply.github.com>
2024-03-31 14:27:59 +0800

    Introduce sqlite3 connect

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import sqlite3
 
 app = Flask(__name__)
 
+DATABASE = 'database/main.db'
+
+def get_db():
+    dbconnect = getattr(g, '_database', None)
+    if dbconnect is None:
+        dbconnect = g._database = sqlite3.connect(DATABASE)
+    return dbconnect
+
 @app.route("/")
 def hello_world():
     return render_template('index.html')
