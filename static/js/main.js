bedebc127fecf9a01b84fd5a20411b430d3aab55
xosadmin <9164356+xosadmin@users.noreply.github.com>
2024-04-09 21:25:30 +0800

    Adapt Javascript & Fix minor errors

--- static/js/main.js
+++ static/js/main.js
@@ -19,11 +19,8 @@ function loadLLM(flag) {
     
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector('form');
-
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('doSubmit', function(event) {
         let isValid = true;
-
-        
         function checkNotEmpty(inputId, message) {
             const input = document.getElementById(inputId);
             if (!input || input.value.trim().length === 0) {
@@ -31,8 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 isValid = false;
             }
         }
-
-       
+        
         function validateEmail() {
             const email = document.getElementById('email');
             if (email && (email.value.length === 0 || !email.value.includes('@'))) {
@@ -41,7 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
 
-        
         function validatePasswordLength() {
             const password = document.getElementById('password');
             if (password && password.value.length < 8) {
@@ -50,7 +45,6 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
 
-        
         function validatePasswordMatch() {
             const password = document.getElementById('password');
             const repeatPassword = document.getElementById('repeat-password');
@@ -59,23 +53,19 @@ document.addEventListener("DOMContentLoaded", function() {
                 isValid = false;
             }
         }
-
        
         validateEmail();
         validatePasswordLength();
 
-        
         if (document.getElementById('repeat-password') !== null) {
             validatePasswordMatch();
         }
 
-        
         checkNotEmpty('content', 'Content cannot be empty.');
         checkNotEmpty('title', 'Title cannot be empty.');
         checkNotEmpty('rewards', 'Rewards cannot be empty.');
         checkNotEmpty('timelimit', 'Time Limit cannot be empty.');
 
-        
         if (!isValid) {
             event.preventDefault();
         }
