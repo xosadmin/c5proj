ce24756682b3e1b6dd6ba9af21ef19a0afd86aaf
root <tuteysms@gmail.com>
2024-04-14 15:49:13 +0800

    adding some unity test

--- tests/requests_test.py
+++ tests/requests_test.py
@@ -1,49 +1,56 @@
 import unittest
-from ..app import donewthreads,donewthreadreply
+import app
 from flask import Flask
 
-class TestDonewThreads(unittest.TestCase):
+class TestRequestOperations(unittest.TestCase):
 
     def setUp(self):
         self.app = Flask(__name__).test_client()
         self.app.testing = True
 
-    def test_donewthreads_post_success(self):
-       with self.app as client:
-            response = client.post('/donewthread', data={
-                'title': 'New Thread Title',
-                'content': 'This is the content of the new thread.'
+    def test_donewrequests_post_success(self):
+        with self.app as client:
+            response = client.post('/donewrequest', data={
+                'title': 'New Request Title',
+                'content': 'This is the content of the new request.',
+                'rewards': '100',
+                'timelimit': '7'
             })
-           
-            self.assertEqual(response.status_code, 302)  
-            self.assertTrue('communityPage' in response.location)  
-    
-    def test_donewthreads_get_invalid(self):
-        
-        with self.app as client:
-            response = client.get('/donewthread')
-            
-            self.assertEqual(response.status_code, 302) 
+            self.assertEqual(response.status_code, 302)
+            self.assertTrue('requests' in response.location)
 
-     def test_doNewThreadReply_post_success(self):
-        
+    def test_donewrequests_get_invalid(self):
         with self.app as client:
-            response = client.post('/donewthreadreply', data={
-                'threadID': 'some-unique-thread-id',
-                'content': 'Reply content for the thread.'
+            response = client.get('/donewrequest')
+            self.assertEqual(response.status_code, 302)
+            self.assertTrue('newRequest?msg=Invalid Request!' in response.location)
+
+    def test_doAcceptRequest_post_success(self):
+        with self.app as client:
+            response = client.post('/doacceptrequest/valid-request-id')
+            self.assertEqual(response.status_code, 302)
+            self.assertTrue('todoList' in response.location)
+
+    def test_doAcceptRequest_get_invalid(self):
+        with self.app as client:
+            response = client.get('/doacceptrequest/valid-request-id')
+            self.assertEqual(response.status_code, 302)  
+
+    def test_doAnswerRequest_post_success(self):
+        with self.app as client:
+            response = client.post('/doanswerrequest', data={
+                'userID': 'valid-user-id',
+                'rewards': '50',
+                'requestID': 'valid-request-id',
+                'content': 'This is the answer content for the request.'
             })
-           
-            self.assertEqual(response.status_code, 302)  
-            self.assertTrue('communityPage' in response.location)  
+            self.assertEqual(response.status_code, 302)
+            self.assertTrue('todoList' in response.location)
 
-    def test_doNewThreadReply_get_invalid(self):
-        
+    def test_doAnswerRequest_get_invalid(self):
         with self.app as client:
-            response = client.get('/donewthreadreply')
+            response = client.get('/doanswerrequest')
             self.assertEqual(response.status_code, 302)  
-            self.assertTrue('newThread?errmsg=Invalid Request!' in response.location)  
- 
-            
 
 if __name__ == '__main__':
     unittest.main()
