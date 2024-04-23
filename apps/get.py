676feb5dbfa7dfa09e0d738ab3b2bd1f1b604bd2
xosadmin <9164356+xosadmin@users.noreply.github.com>
2024-04-23 18:43:46 +0800

    Add get_test unit test module

--- apps/get.py
+++ apps/get.py
@@ -9,7 +9,6 @@ from app import *
 import hashlib
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.getcwd() + '/database/main.db'
 
 def checkEmail(email):
         user = UserInfo.query.filter(UserInfo.email == email).first()
@@ -112,11 +111,11 @@ def ifSign(userID):
      else:
           return False # If the user did not sign today
 
-def getTime():
-    currentTime = datetime.now()
-    composeTime = str(currentTime.year) + "-" + str(currentTime.month) + "-" + str(currentTime.day) + " on " + \
-                  str(currentTime.hour) + ":" + str(currentTime.minute) + ":" + str(currentTime.second)
-    return composeTime
+# def getTime():
+#     currentTime = datetime.now()
+#     composeTime = str(currentTime.year) + "-" + str(currentTime.month) + "-" + str(currentTime.day) + " on " + \
+#                   str(currentTime.hour) + ":" + str(currentTime.minute) + ":" + str(currentTime.second)
+#     return composeTime
 
 def encryptPassword(text):
      md5 = hashlib.md5()
--- tests/get_test.py
+++ tests/get_test.py
@@ -1,19 +1,22 @@
 import unittest
 from apps.get import *
-from flask import *
+from app import app, db
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 class TestGETPY(unittest.TestCase):
     def setUp(self):
-        app = Flask(__name__)
-        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(os.getcwd(), 'database', 'main.db')
-        self.app = app  # Store the app instance for testing
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
     def test_checkEmail(self):
         with self.app.app_context():
             Input = "test@test.com" # Email address that existed in the system
             result = checkEmail(Input)
-            self.assertEqual(result,"-1")
+            self.assertEqual(result,-1)
     
     def test_getThreadTitle(self):
         with self.app.app_context():
@@ -25,56 +28,56 @@ class TestGETPY(unittest.TestCase):
         with self.app.app_context():
             Input = "b56b9e05-0823-480b-aace-e14c698e99b9" # UserID
             result = getCoins(Input)
-            self.assertEqual(result,"10")
+            self.assertEqual(result,10)
 
     def test_getRequestInfo(self):
         with self.app.app_context():
             Input1 = 21 # RequestID
             testCases = ["userID","state","rewards"]
             expectAnswers = ["0", "Completed", "123"] # [userid, status, rewards]
-            for i in range(0,testCases):
+            for i in range(0,len(testCases)):
                 result = getRequestInfo(Input1,testCases[i])
                 if result != expectAnswers[i]:
-                    self.assertFalse()
-            self.assertTrue()
+                    self.assertFalse(False)
+            self.assertTrue(True)
     
     def test_getUserInfo(self):
         with self.app.app_context():
             input1 = "0" # User ID
             testCases = ["email","pincode","coins","avatar"]
             expectAnswers = ["test2@test2.com","1234","10","default"]
-            for i in range(0,testCases):
+            for i in range(0,len(testCases)):
                 result = getUserInfo(input1,testCases[i])
                 if result != expectAnswers[i]:
-                    self.assertFalse()
-            self.assertTrue()
+                    self.assertFalse(False)
+            self.assertTrue(True)
 
     def test_verifyPinCode(self):
         with self.app.app_context():
             inputs = ["0","test@test.com"] # User ID and Email Address
             for i in range(0,len(inputs)):
                 if verifyPinCode(inputs[i],"1234") != 0:
-                    self.assertFalse()
-            self.assertTrue()
+                    self.assertFalse(False)
+            self.assertTrue(True)
 
     def test_ifUserPurchased(self):
         with self.app.app_context():
             input1 = "0" # UserID
             input2 = "1" # ItemID
             if ifUserPurchased(input1,input2) == 0:
-                self.assertTrue()
+                self.assertTrue(True)
             else:
-                self.assertFalse()
+                self.assertFalse(False)
     
     def test_getItemInfo(self):
         with self.app.app_context():
             input1 = "1"
             testCases = ["detail","price"]
             expectAnswers = ["The Man with Prime","100"]
-            for i in range(0,testCases):
+            for i in range(0,len(testCases)):
                 if getItemInfo(input1,testCases[i]) != expectAnswers[i]:
-                    self.assertFalse()
-            self.assertTrue()
+                    self.assertFalse(False)
+            self.assertTrue(True)
     
     def test_ifSign(self):
         with self.app.app_context():
