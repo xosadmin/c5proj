9271ca3030aed2e63f3e7f62b8e0af15f2adf954
xosadmin <9164356+xosadmin@users.noreply.github.com>
2024-05-07 18:22:18 +0800

    Introduce Registry Form Validation by jQuery

--- static/js/register.js
+++ static/js/register.js
@@ -1,16 +1,55 @@
 $(document).ready(function(){
-    $("#repeat_password").on('change',function(){
+    function controlSubmit(flag, value){
+        if (flag === 1) { // Disable submit
+            var notify = "<p>Submit Disabled: ";
+            notify = notify + value + "</p>";
+            $("#dosubm").prop("disabled",true);
+            $("#whydisableSubButt").html(notify);
+            $("#whydisableSubButt").show();
+        }
+        else if (flag === 2) { // Recover submit button
+            $("#dosubm").prop("disabled",false);
+            $("#whydisableSubButt").hide();
+        }
+        else {
+            console.error("Invalid Flag!");
+        }
+    }
+
+    $("#email").on('change',function(){
+        var emailInput = $("#email").val();
+        if (emailInput.indexOf("@") === -1 || emailInput.indexOf(".") === -1) { // If user input invalid email address
+            var notify = "Invalid Email Address!";
+            controlSubmit(1,notify);
+            alert(notify);
+        }
+        else {
+            controlSubmit(2,"");
+        }
+    });
+
+    $("#pin_code").on('change',function(){
+        var pincodeInput = $("#pin_code").val();
+        if (pincodeInput.length < 4) {
+            var notify = "Invalid PIN Code!";
+            controlSubmit(1,notify);
+            alert(notify);
+        }
+        else {
+            controlSubmit(2,"");
+        }
+    });
+
+    $("#repeat_password").on('change',function(){ // Check repeat password
         var passwordValue = $("#password").val();
         var repeatValue = $(this).val();
         if (passwordValue !== repeatValue){
-            $("#dosubm").prop("disabled",true);
-            $("#whydisableSubButt").html("<p>Submit Disabled: The password is not equal to repeat password.</p>");
-            $("#whydisableSubButt").show();
-            alert("Password Mismatch!");
+            var notify = "The password is not equal to repeat password.";
+            controlSubmit(1,notify);
+            alert(notify);
         }
         else {
-            $("#dosubm").prop("disabled",false);
-            $("#whydisableSubButt").hide();
+            controlSubmit(2,"");
         }
     });
 });
